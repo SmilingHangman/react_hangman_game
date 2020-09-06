002b5577acfe7dbd46d96ea742ea9595113f150b
SmilingHangman <andrejus.chomicenka@gmail.com>
2020-09-06 10:52:17 +0300

    Updates

---
+++ precache-manifest.67feca7a16408a31da446da8273d4a4a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bab9ac668adff3873c1f8bf5eef06ca2",
+    "url": "/react_hangman_game/index.html"
+  },
+  {
+    "revision": "c52efa4c17d75ddb4957",
+    "url": "/react_hangman_game/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "9940c80a97ea20ffd32d",
+    "url": "/react_hangman_game/static/css/main.50b41248.chunk.css"
+  },
+  {
+    "revision": "c52efa4c17d75ddb4957",
+    "url": "/react_hangman_game/static/js/2.f541e5ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_hangman_game/static/js/2.f541e5ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9940c80a97ea20ffd32d",
+    "url": "/react_hangman_game/static/js/main.b639907a.chunk.js"
+  },
+  {
+    "revision": "caa71fcfd015c28a9b88",
+    "url": "/react_hangman_game/static/js/runtime-main.47bd2fac.js"
+  },
+  {
+    "revision": "0fce98babaa343d657a168f1ce77a97b",
+    "url": "/react_hangman_game/static/media/dedface.0fce98ba.svg"
+  },
+  {
+    "revision": "e8d2cbec254fb172f716a7ed7aec7c27",
+    "url": "/react_hangman_game/static/media/gallows.e8d2cbec.png"
+  },
+  {
+    "revision": "2e848affae67d6695ac649fabc67678f",
+    "url": "/react_hangman_game/static/media/hangmans_knot.2e848aff.svg"
+  },
+  {
+    "revision": "e90b4c45ab87446ddca71b473fc5dd95",
+    "url": "/react_hangman_game/static/media/neutralface.e90b4c45.svg"
+  },
+  {
+    "revision": "5564f630bf698329637ce2cba6a55198",
+    "url": "/react_hangman_game/static/media/prededface.5564f630.svg"
+  },
+  {
+    "revision": "75ce344bab30506c83f4e30fd5f59f75",
+    "url": "/react_hangman_game/static/media/sadface.75ce344b.svg"
+  },
+  {
+    "revision": "57eb1cd158f67b7b11a5f666bb7f7d91",
+    "url": "/react_hangman_game/static/media/screamingface.57eb1cd1.svg"
+  },
+  {
+    "revision": "ca499cc3a23506a96d0e1354048f2a8c",
+    "url": "/react_hangman_game/static/media/tearface.ca499cc3.svg"
+  }
+]);
